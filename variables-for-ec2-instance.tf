b8bbf8fb4f0ce17a0d4c182b5061b85700a09475
alokaryan <alokaryan@gmail.com>
2020-06-07 15:44:58 +0530

    variables for ec2 instance

---
+++ variables-for-ec2-instance.tf
@@ -0,0 +1,34 @@
+# filename - variables.tf
+variable "ami" {
+	default = "ami-0447a12f28fddb066"
+	description = "AMI to use"
+}
+
+#file end here
+/*
+now create one more file that will 
+use the variables, call it base.tf
+with these contents
+*/
+
+# filename - base.tf
+provider "aws" {
+	region = "ap-south-1"
+
+}
+
+resource "aws_instance" "base" {
+	ami = var.ami
+	instance_type = "t2.micro"
+}
+
+/*
+put both the files in a single directory
+then run
+terraform validate
+terraform plan
+terraform apply
+*/
+
+
+
