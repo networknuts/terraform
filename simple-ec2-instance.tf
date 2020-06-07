ec685c3d8bb01b7c0bf502bed2c0809c9a959335
alokaryan <alokaryan@gmail.com>
2020-06-07 13:06:27 +0530

    simple ec2 instance

---
+++ simple-ec2-instance.tf
@@ -0,0 +1,24 @@
+/* this is our first file
+created using terraform and
+god bless you */
+
+provider "aws" {
+  access_key = "your aws access key here"
+  secret_key = "your aws secret key here"
+  region     = "ap-south-1"
+}
+
+
+resource "aws_instance" "ourfirst" {
+  ami           = "ami-0447a12f28fddb066"
+  instance_type = "t2.micro"
+}
+
+/*
+run
+terraform validate
+terraform plan
+terraform apply
+and after checking the instance on aws dashboard
+terraform destroy
+*/
