5c5f8ae1895621a9971b75312c77609dc7ff0210
alokaryan <alokaryan@gmail.com>
2021-10-21 15:22:23 +0530

    create and destroy local provisioner

--- local-provisioner.tf
+++ local-provisioner.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "ap-south-1" #mumbai region
 }
 
-
 resource "aws_instance" "sampleserver" {
   ami = "ami-0c1a7f89451184c8b" #this ami is specific to mumbai region
   instance_type = "t2.micro"
