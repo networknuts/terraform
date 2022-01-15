c570060864f5e007a2cfae61105d98cc02c3f586
alokaryan <alokaryan@gmail.com>
2022-01-15 12:43:44 +0530

    Update simple-ec2-instance.tf

--- simple-ec2-instance.tf
+++ simple-ec2-instance.tf
@@ -12,8 +12,12 @@ provider "aws" {
 resource "aws_instance" "ourfirst" {
   ami           = "ami-0447a12f28fddb066"
   instance_type = "t2.micro"
-}
+lifecycle {
+  prevent_destroy = true
+  ignore_changes = ["ami",]
+  }
 
+}
 /*
 run
 terraform validate
