ac4c9507e33723b4f332b6448ba7ed1821663f71
alokaryan <alokaryan@gmail.com>
2022-06-02 13:49:10 +0530

    create custom condition checks

---
+++ custom-condition-checks.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "example" {
+  instance_type = "t2.micro"
+  ami           = "ami-abc123"
+
+  lifecycle {
+    # The AMI ID must refer to an AMI that contains an operating system
+    # for the `x86_64` architecture.
+    precondition {
+      condition     = data.aws_ami.example.architecture == "x86_64"
+      error_message = "The selected AMI must be for the x86_64 architecture."
+    }
+  }
+}
