6b944adc57a65500c5bd9b1fe442f97b99f42b20
alokaryan <alokaryan@gmail.com>
2022-10-29 11:47:17 +0530

    multiple local exec

--- local-provisioner.tf
+++ local-provisioner.tf
@@ -16,9 +16,19 @@ resource "aws_instance" "sampleserver" {
 
 #if you want to send output to a file
   #this will run at creation
-#  provisioner "local-exec" {
-#     command = "echo ${self.private_ip} > file_${aws_instance.sampleserver.id}.txt"
-#  }
+  provisioner "local-exec" {
+     command = "echo ${self.private_ip} > file_${aws_instance.sampleserver.id}.txt"
+  }
+  
+#multiple times provisioner can be defined
+    provisioner "local-exec" {
+     working_dir = "/tmp"
+     command = "echo ${aws_instance.sampleserver.private_ip} >> sampleserver-ips.txt"
+  }
+  
+    provisioner "local-exec" {
+     command = "echo ${aws_instance.sampleserver.private_ip} >> /tmp/sampleserver-ips.txt"
+  }
   
 #this will run at destroy
   provisioner "local-exec" {
