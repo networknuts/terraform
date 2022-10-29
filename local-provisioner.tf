0d86a0a4974ad0f38ddf391c47d5d364f6756891
alokaryan <alokaryan@gmail.com>
2022-10-29 11:34:24 +0530

    local provisioner with output to a file

--- local-provisioner.tf
+++ local-provisioner.tf
@@ -14,6 +14,12 @@ resource "aws_instance" "sampleserver" {
      command = "echo Server IP address is ${self.private_ip}"
   }
 
+#if you want to send output to a file
+  #this will run at creation
+#  provisioner "local-exec" {
+#     command = "echo ${self.private_ip} > file_${aws_instance.sampleserver.id}.txt"
+#  }
+  
 #this will run at destroy
   provisioner "local-exec" {
      when = destroy
