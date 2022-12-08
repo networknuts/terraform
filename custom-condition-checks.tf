c2cf98a835f443b012caeec0ca9b58d04c583fe2
alokaryan <alokaryan@gmail.com>
2022-12-08 17:41:15 +0530

    Update custom-condition-checks.tf

--- custom-condition-checks.tf
+++ custom-condition-checks.tf
@@ -1,13 +1,25 @@
-resource "aws_instance" "example" {
-  instance_type = "t2.micro"
-  ami           = "ami-abc123"
+provider "aws" {
+  region = "ap-south-1" #mumbai region
+}
 
+data "aws_ami" "example" {
+  owners = ["amazon"]
+  filter {
+    name   = "image-id"
+    values = ["ami-019774e5caffd1685"] #this is a arm architecture ami
+  }
+}
+
+resource "aws_instance" "example" {
+  ami = data.aws_ami.example.id
+  instance_type = "t2.micro"
   lifecycle {
-    # The AMI ID must refer to an AMI that contains an operating system
-    # for the `x86_64` architecture.
     precondition {
       condition     = data.aws_ami.example.architecture == "x86_64"
       error_message = "The selected AMI must be for the x86_64 architecture."
     }
   }
+  tags = {
+     Name = "india-server"
+   }
 }
